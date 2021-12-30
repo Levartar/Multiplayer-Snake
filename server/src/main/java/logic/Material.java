deeb2b9ce98504ec03f8f5789bbc06d5f1f25e2b
Eberl Timo <te038@hdm-stuttgart.de>
2021-12-30 19:01:05 +0100

    added Materials and useful methods

--- server/src/main/java/logic/Material.java
+++ server/src/main/java/logic/Material.java
@@ -1,20 +1,36 @@
 package logic;
 
 public enum Material {
-    WALL ('#'),
-    FREESPACE (' '),
-    APPLE ('@');
+    WALL('#'),
+    FREESPACE(' '),
+    APPLE('@'),
+    SNAKE('o'),
+    SPAWN('+');
 
-    private final char material;
+    private final char symbol;
 
     Material(char symbol) {
 
-        this.material = symbol;
+        this.symbol = symbol;
 
     }
 
     public char getSymbol() {
-        return this.material;
+        return this.symbol;
+    }
+
+    @Override
+    public String toString() {
+        return Character.toString(getSymbol());
+    }
+
+    public static Material getMaterial(char symbol) {
+        for (Material material : values()) {
+            if (symbol == material.symbol) {
+                return material;
+            }
+        }
+        return null;
     }
 }
 
