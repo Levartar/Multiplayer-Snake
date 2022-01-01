f3f07e2116ea31f2281d691d7fce111b0ddc3890
Eberl Timo <te038@hdm-stuttgart.de>
2022-01-01 20:31:19 +0100

    added test for Position.add(Direction) and hashCode()

--- server/src/test/java/logic/PositionTest.java
+++ server/src/test/java/logic/PositionTest.java
@@ -22,6 +22,25 @@ class PositionTest {
             Position expected = new Position(2, 6);
             assertEquals(expected, actual);
         }
+        {
+            Position actual = new Position(1, 1);
+
+            actual.add(Direction.right);
+            Position expected = new Position(2, 1);
+            assertEquals(expected, actual);
+
+            actual.add(Direction.down);
+            expected = new Position(2, 2);
+            assertEquals(expected, actual);
+
+            actual.add(Direction.left);
+            expected = new Position(1, 2);
+            assertEquals(expected, actual);
+
+            actual.add(Direction.up);
+            expected = new Position(1, 1);
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
@@ -39,6 +58,22 @@ class PositionTest {
 
             Position expected = new Position(3, 5);
             assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void testHashCode() {
+        {
+            int hash1 = new Position(1, 2).hashCode();
+            int hash2 = new Position(2, 1).hashCode();
+
+            assertNotEquals(hash1, hash2);
+        }
+        {
+            int hash1 = new Position(1, 1).hashCode();
+            int hash2 = new Position(1, 1).hashCode();
+
+            assertEquals(hash1, hash2);
         }
     }
 }
