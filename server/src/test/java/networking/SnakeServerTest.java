e35c253100c6fa9ecc0c63b7dc2cabc53120dc5d
Eberl Timo <te038@hdm-stuttgart.de>
2022-01-03 19:59:50 +0100

    deleted indefinitely running test

--- server/src/test/java/networking/SnakeServerTest.java
+++ server/src/test/java/networking/SnakeServerTest.java
@@ -3,10 +3,4 @@ package networking;
 import org.junit.jupiter.api.Test;
 
 class SnakeServerTest {
-
-    @Test
-    void run() {
-        SnakeServer server = new SnakeServer(80);
-        server.run();
-    }
 }
