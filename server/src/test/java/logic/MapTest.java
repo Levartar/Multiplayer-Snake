b4f4b1bddec9f496ee38770c6e15331ff680eb0f
Eberl Timo <te038@hdm-stuttgart.de>
2022-01-11 21:22:37 +0100

    new map test

--- server/src/test/java/logic/MapTest.java
+++ server/src/test/java/logic/MapTest.java
@@ -10,6 +10,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class MapTest {
 
     @Test
+    void getMaterialAt() {
+        String testMap = """
+                #####
+                # @ #
+                #   #
+                #   #
+                #  ##""";
+        Map squareMap = new Map(testMap);
+        assertEquals('#', squareMap.getMaterialAt(new Position(0, 0)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(1, 0)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(2, 0)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(3, 0)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(4, 0)).getSymbol());
+
+        assertEquals('#', squareMap.getMaterialAt(new Position(0, 1)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(1, 1)).getSymbol());
+        assertEquals('@', squareMap.getMaterialAt(new Position(2, 1)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(3, 1)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(4, 1)).getSymbol());
+
+        assertEquals('#', squareMap.getMaterialAt(new Position(0, 2)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(1, 2)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(2, 2)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(3, 2)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(4, 2)).getSymbol());
+
+        assertEquals('#', squareMap.getMaterialAt(new Position(0, 3)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(1, 3)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(2, 3)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(3, 3)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(4, 3)).getSymbol());
+
+        assertEquals('#', squareMap.getMaterialAt(new Position(0, 4)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(1, 4)).getSymbol());
+        assertEquals(' ', squareMap.getMaterialAt(new Position(2, 4)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(3, 4)).getSymbol());
+        assertEquals('#', squareMap.getMaterialAt(new Position(4, 4)).getSymbol());
+    }
+
+    @Test
     void loadMap() {
         //Build TestString
         String testMap = """
