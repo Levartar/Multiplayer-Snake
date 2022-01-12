53dde1d42f1aba8cbba770c2cfc70a3e0e390724
Eberl Timo <te038@hdm-stuttgart.de>
2022-01-12 12:36:50 +0100

    added timer

--- server/src/main/java/SwingApp.java
+++ server/src/main/java/SwingApp.java
@@ -73,7 +73,6 @@ public class SwingApp extends JFrame implements KeyListener {
                 #                                  #
                 #                                  #
                 #                                  #
-                #                                  #
                 ####################################""";
         Map map = new Map(mapString);
 
@@ -105,7 +104,7 @@ public class SwingApp extends JFrame implements KeyListener {
 
     private void render() {
         if (gameOver) textPane.setText("Game Over!");
-        else textPane.setText(TUTORIAL_TEXT + gamemode.toString());
+        else textPane.setText(TUTORIAL_TEXT + "time left: " + gamemode.getTimer() + " seconds\n" + gamemode.toString());
     }
 
     @Override
