ae203a9d799cf5d1f528a27d28edf98c13ded761
Eberl Timo <te038@hdm-stuttgart>
2022-01-13 08:42:21 +0100

    change Swing window height

--- server/src/main/java/SwingApp.java
+++ server/src/main/java/SwingApp.java
@@ -41,7 +41,7 @@ public class SwingApp extends JFrame implements KeyListener {
     }
 
     private void swinit() {
-        setBounds(0, 0, 1000, 600);
+        setBounds(0, 0, 1000, 650);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         textPane = new JTextPane();
         textPane.setBackground(Color.BLACK);
