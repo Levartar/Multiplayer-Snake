f1bf2e8bc9906dd8b702a738610de54f7ad76f5a
Nikola Hein <nh096@hdm-stuttgart.de>
2022-01-14 18:06:15 +0100

    added log to DatabaseTest

--- server/src/test/java/database/DatabaseTest.java
+++ server/src/test/java/database/DatabaseTest.java
@@ -9,17 +9,19 @@ import Database.SQLConnection;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
- //  private static final Logger log = LogManager.getLogger(SQLConnection.class);
- //  String name = "Jens";
- //  int highscore = 2048;
- //  @Test
- //  void postDatatest() {
- //      assertTrue(sendData());
- //  }
- //  @Test
- //  void receiveDatatest() {
- //      assertEquals(highscore, Integer.valueOf(receiveData()));
- //  }
+   private static final Logger log = LogManager.getLogger(SQLConnection.class);
+   String name = "Jens";
+   int highscore = 2048;
+   @Test
+   void postDatatest() {
+       assertTrue(sendData());
+       log.info("send Data to Database!");
+   }
+   @Test
+   void receiveDatatest() {
+       assertEquals(highscore, Integer.valueOf(receiveData()));
+       log.info("received Data");
+   }
 
    // @Test
    // void deleteDatatest(){
@@ -35,30 +37,27 @@ public class DatabaseTest {
   // }
 
 
+  public String receiveData(){
+      String returnString = "";
+      try {
+          ResultSet resultSet = SQLConnection.executeMyQuery("Select highscore from highscores where Name = '" + name + "';", "testdb");
+          while (resultSet.next()) {
+              returnString = resultSet.getString(1);
+          }
+      } catch (SQLException e) {
+          log.info(e);
+      }
+      SQLConnection.closeConnections();
+      return returnString;
+  }
+  public boolean sendData(){
 
+      return (SQLConnection.InsertSnakeHighscore(name,highscore));
+  }
 
-
- //  public String receiveData(){
- //      String returnString = "";
- //      try {
- //          ResultSet resultSet = SQLConnection.executeMyQuery("Select highscore from highscores where Name = '" + name + "';", "testdb");
- //          while (resultSet.next()) {
- //              returnString = resultSet.getString(1);
- //          }
- //      } catch (SQLException e) {
- //          log.info(e);
- //      }
- //      SQLConnection.closeConnections();
- //      return returnString;
- //  }
- //  public boolean sendData(){
-
- //      return (SQLConnection.InsertSnakeHighscore(name,highscore));
- //  }
-
- //  public boolean deleteData(){
- //      return (SQLConnection.deleteHighscore(name, "testdb"));
- //  }
+  public boolean deleteData(){
+      return (SQLConnection.deleteHighscore(name, "testdb"));
+  }
 
 
 }
