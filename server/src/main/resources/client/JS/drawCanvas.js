4be3e57dc39c5b632863621961c5e07e7787fa52
Bauer Tim <tb166@hdm-stuttgart.de>
2022-01-25 15:17:07 +0100

    changes to the rendering of the assets

--- server/src/main/resources/client/JS/drawCanvas.js
+++ server/src/main/resources/client/JS/drawCanvas.js
@@ -22,7 +22,7 @@ function drawWorld(world, cellSize){
     const canvasMap = document.getElementById("mapCanvas")
     const context = canvasMap.getContext("2d")
     const atlasImage = new Image();   // Create new img element
-    atlasImage.src = '../assets/atlasImage.png'; // Set source path
+    atlasImage.src = "./assets/snakeAtlas.png"; // Set source path
 
     let x = 0
     let y = 0
@@ -64,29 +64,9 @@ function drawSnakes(snakes, cellSize){
     const canvasMap = document.getElementById("mapCanvas")
     const context = canvasMap.getContext("2d")
     const atlasImage = new Image();   // Create new img element
-    atlasImage.src = '../assets/atlasImage.png'; // Set source path
+    atlasImage.src = "./assets/snakeAtlas.png"; // Set source path
 
     for (let i = 0; i < snakes.length; i++) {
-        //draw Head
-        if (snakes[i].positions.length>0){ //check if snake has a head to prevent errors
-            let x = snakes[i].positions[0].x
-            let y = snakes[i].positions[0].y
-            switch (snakes[i].direction){
-                case "up":
-                    context.drawImage(atlasImage, 0, 20, 10, 10, x, y, cellSize, cellSize); //HeadUp
-                    break;
-                case "down":
-                    context.drawImage(atlasImage, 10, 20, 10, 10, x, y, cellSize, cellSize); //HeadDown
-                    break;
-                case "left":
-                    context.drawImage(atlasImage, 30, 20, 10, 10, x, y, cellSize, cellSize); //HeadLeft
-                    break;
-                case "right":
-                    context.drawImage(atlasImage, 20, 20, 10, 10, x, y, cellSize, cellSize); //HeadRight
-                    break;
-            }
-        }
-
         //draw Body
         for (let j = 1; j < snakes[i].positions.length-1; j++) {
 
@@ -94,72 +74,93 @@ function drawSnakes(snakes, cellSize){
             let y = snakes[i].positions[j].y
             let after = snakes[i].positions[j+1]
             let prior = snakes[i].positions[j-1]
+            let direction
 
-            if (Math.abs(x-after.x)==1&&Math.abs(x-prior.x)==1){let direction = "sideways"} else
-            if (Math.abs(y-after.y)==1&&Math.abs(y-prior.y)==1){let direction = "straight"} else
-            if (x-after.x==-1&&y-prior.y==-1){let direction = "downToRight"} else
-            if (x-after.x==1&&y-prior.y==-1){let direction = "downToLeft"} else
-            if (x-after.x==-1&&y-prior.y==1){let direction = "upToRight"} else
-            if (x-after.x==1&&y-prior.y==1){let direction = "upToLeft"} else
-            {let direction = "nd"}
+            if (Math.abs(x-after.x)===1&&Math.abs(x-prior.x)===1){direction = "sideways"} else
+            if (Math.abs(y-after.y)===1&&Math.abs(y-prior.y)===1){direction = "straight"} else
+            if (x-after.x===-1&&y-prior.y===-1){direction = "downToLeft"} else
+            if (x-after.x===1&&y-prior.y===-1){direction = "downToRight"} else
+            if (x-after.x===-1&&y-prior.y===1){direction = "upToLeft"} else
+            if (x-after.x===1&&y-prior.y===1){direction = "upToRight"} else
+            {direction = "nd"}
             switch (direction){
                 case "sideways":
-                    context.drawImage(atlasImage, 10, 0, 10, 10, x, y, cellSize, cellSize); //TailUp
+                    context.drawImage(atlasImage, 10, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailUp
                     break;
                 case "straight":
-                    context.drawImage(atlasImage, 0, 0, 10, 10, x, y, cellSize, cellSize); //TailDown
+                    context.drawImage(atlasImage, 0, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailDown
                     break;
                 case "downToRight":
-                    context.drawImage(atlasImage, 40, 0, 10, 10, x, y, cellSize, cellSize); //TailLeft
+                    context.drawImage(atlasImage, 40, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailLeft
                     break;
                 case "downToLeft":
-                    context.drawImage(atlasImage, 20, 0, 10, 10, x, y, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 20, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "upToRight":
-                    context.drawImage(atlasImage, 50, 0, 10, 10, x, y, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 50, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "upToLeft":
-                    context.drawImage(atlasImage, 40, 0, 10, 10, x, y, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 40, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "nd":
-                    context.drawImage(atlasImage, 0, 10, 10, 10, x, y, cellSize, cellSize); //Bubble
+                    context.drawImage(atlasImage, 0, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //Bubble
                     break;
             }
-            context.fillStyle = snakes[i].color
-            context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
+            //context.fillStyle = snakes[i].color
+            //context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize)
         }
 
         //draw Tail
-        if (typeof snkaes[i].positions[snakes[i].positions.length-1] != "undefined"){ //Tail exists
+        if (typeof snakes[i].positions[snakes[i].positions.length-1] != "undefined"){ //Tail exists
             let tail = snakes[i].positions[snakes[i].positions.length-1]
             let x = tail.x
             let y = tail.y
+            let direction
             //get prior direction
             let prior = snakes[i].positions[snakes[i].positions.length-2]
-            if (prior.x - tail.x == -1){let direction = "right"} else
-            if (prior.x - tail.x == 1){let direction = "left"} else
-            if (prior.y - tail.y == -1){let direction = "up"} else
-            if (prior.y - tail.y == 1){let direction = "down"} else
-            {let direction = "nd"}
+            if (prior.x - tail.x == -1){direction = "right"} else
+            if (prior.x - tail.x == 1){direction = "left"} else
+            if (prior.y - tail.y == -1){direction = "up"} else
+            if (prior.y - tail.y == 1){direction = "down"} else
+            {direction = "nd"}
             switch (direction){
                 case "up":
-                    context.drawImage(atlasImage, 20, 10, 10, 10, x, y, cellSize, cellSize); //TailUp
+                    context.drawImage(atlasImage, 20, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailUp
                     break;
                 case "down":
-                    context.drawImage(atlasImage, 10, 10, 10, 10, x, y, cellSize, cellSize); //TailDown
+                    context.drawImage(atlasImage, 10, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailDown
                     break;
                 case "left":
-                    context.drawImage(atlasImage, 30, 10, 10, 10, x, y, cellSize, cellSize); //TailLeft
+                    context.drawImage(atlasImage, 30, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailLeft
                     break;
                 case "right":
-                    context.drawImage(atlasImage, 40, 10, 10, 10, x, y, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 40, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "nd":
-                    context.drawImage(atlasImage, 0, 10, 10, 10, x, y, cellSize, cellSize); //Bubble
+                    context.drawImage(atlasImage, 0, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //Bubble
                     break;
             }
         }
 
+        //draw Head
+        if (snakes[i].positions.length>0){ //check if snake has a head to prevent errors
+            let x = snakes[i].positions[0].x
+            let y = snakes[i].positions[0].y
+            switch (snakes[i].direction){
+                case "up":
+                    context.drawImage(atlasImage, 0, 20, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //HeadUp
+                    break;
+                case "down":
+                    context.drawImage(atlasImage, 10, 20, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //HeadDown
+                    break;
+                case "right":
+                    context.drawImage(atlasImage, 30, 20, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //HeadLeft
+                    break;
+                case "left":
+                    context.drawImage(atlasImage, 20, 20, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //HeadRight
+                    break;
+            }
+        }
 
         // draw the names of the players over their names
         context.font = "16px Arial"
