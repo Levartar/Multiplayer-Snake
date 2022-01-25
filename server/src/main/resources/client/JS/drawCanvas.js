3557eb5e457797b6619b87c2278c28f92b4c6bb7
Jakob Günster <jg160@hdm-stuttgart.de>
2022-01-25 19:48:06 +0100

    Working responsive directions

--- server/src/main/resources/client/JS/drawCanvas.js
+++ server/src/main/resources/client/JS/drawCanvas.js
@@ -3,6 +3,7 @@ function drawGrid(bw, bh, cellSize){
     const canvasMap = document.getElementById("mapCanvas")
     const context = canvasMap.getContext("2d")
 
+
     for (let x = 0; x <= bw; x += cellSize) {
         context.moveTo(0.5 + x, 0)
         context.lineTo(0.5 + x, bh)
@@ -35,13 +36,13 @@ function drawWorld(world, cellSize){
                 //context.fillRect(x, y, cellSize, cellSize)
                 //s = source, d = destination
                 //.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-                context.drawImage(atlasImage, 10, 30, 10, 10, x, y, cellSize, cellSize); //Wall
+                context.drawImage(atlasImage, 10, 30, 10, 10, x+.5, y+.5, cellSize, cellSize); //Wall
                 x += cellSize
                 break
             case "@":
                 //context.fillStyle = "red"
                 //context.fillRect(x, y, cellSize, cellSize)
-                context.drawImage(atlasImage, 0, 30, 10, 10, x, y, cellSize, cellSize); //Apple
+                context.drawImage(atlasImage, 0, 30, 10, 10, x+.5, y+.5, cellSize, cellSize); //Apple
                 x += cellSize
                 break
             case " ":
@@ -78,11 +79,21 @@ function drawSnakes(snakes, cellSize){
 
             if (Math.abs(x-after.x)===1&&Math.abs(x-prior.x)===1){direction = "sideways"} else
             if (Math.abs(y-after.y)===1&&Math.abs(y-prior.y)===1){direction = "straight"} else
-            if (x-after.x===-1&&y-prior.y===-1){direction = "downToLeft"} else
-            if (x-after.x===1&&y-prior.y===-1){direction = "downToRight"} else
-            if (x-after.x===-1&&y-prior.y===1){direction = "upToLeft"} else
-            if (x-after.x===1&&y-prior.y===1){direction = "upToRight"} else
-            {direction = "nd"}
+            if (y-after.y===1&&x-prior.x===1){direction = "upToLeft"} else
+            if (y-after.y===1&&x-prior.x===-1){direction = "upToRight"} else
+            if (x-after.x===1&&y-prior.y===-1){direction = "downToLeft"} else
+            if (x-after.x===-1&&y-prior.y===1){direction = "upToRight"} else
+            if (x-after.x===1&&y-prior.y===1){direction = "upToLeft"} else
+            if (y-after.y===-1&&x-prior.x===1){direction = "downToLeft"} else
+            if (y-after.y===1&&x-prior.x===-1){direction = "downToLeft"} else
+            if (x-after.x===-1&&y-prior.y===-1){direction = "downToRight"} else
+            if (y-after.y===-1&&x-prior.x===-1){direction = "downToRight"} else
+
+            {direction = "nd"
+                console.log("x="+x+",y="+y)
+                console.log("after.x="+after.x+",after.y="+after.x)
+                console.log("prior.x="+prior.x+",prior.y="+prior.y)
+            }
             switch (direction){
                 case "sideways":
                     context.drawImage(atlasImage, 10, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailUp
@@ -90,17 +101,17 @@ function drawSnakes(snakes, cellSize){
                 case "straight":
                     context.drawImage(atlasImage, 0, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailDown
                     break;
-                case "downToRight":
+                case "downToLeft":
                     context.drawImage(atlasImage, 40, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailLeft
                     break;
-                case "downToLeft":
+                case "downToRight":
                     context.drawImage(atlasImage, 20, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "upToRight":
-                    context.drawImage(atlasImage, 50, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 30, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "upToLeft":
-                    context.drawImage(atlasImage, 40, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
+                    context.drawImage(atlasImage, 50, 0, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //TailRight
                     break;
                 case "nd":
                     context.drawImage(atlasImage, 0, 10, 10, 10, x * cellSize, y * cellSize, cellSize, cellSize); //Bubble
@@ -163,8 +174,8 @@ function drawSnakes(snakes, cellSize){
         }
 
         // draw the names of the players over their names
-        context.font = "16px Arial"
-        context.fillStyle = "black"
+        context.font = "bolder 16px Arial"
+        context.fillStyle = "white"
         // the '- (snakes[i].name.length * 8)/2 + cellSize/2' is to center the name over the players head
         context.fillText(snakes[i].name, (snakes[i].positions[0].x * cellSize) - (snakes[i].name.length * 8)/2 + cellSize/2, snakes[i].positions[0].y * cellSize, cellSize * 4)
     }
