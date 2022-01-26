9dd8beb2c2d27b92d556c20919f6846a4ce1b353
Jakob Günster <jg160@hdm-stuttgart.de>
2022-01-26 18:36:58 +0100

    New Map Hideouts

--- server/src/main/resources/client/JS/Game.js
+++ server/src/main/resources/client/JS/Game.js
@@ -14,6 +14,7 @@ class Game extends React.Component {
         document.addEventListener('touchstart', handleTouchStart, false);
         document.addEventListener('touchmove', handleTouchMove, false);
 
+        //Mobile Inputs from https://stackoverflow.com/users/7852/givanse
         let xDown = null;
         let yDown = null;
 
