c2c0fad3ead8d2aabe6a93e7ebc48f003210552b
Jakob Günster <jg160@hdm-stuttgart.de>
2022-01-26 17:58:40 +0100

    Mobile Inputs lol

--- server/src/main/resources/client/JS/Game.js
+++ server/src/main/resources/client/JS/Game.js
@@ -11,6 +11,55 @@ class Game extends React.Component {
 
         //send the input of the Player to the backend
         document.addEventListener("keydown", sendInput)
+        document.addEventListener('touchstart', handleTouchStart, false);
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        let xDown = null;
+        let yDown = null;
+
+        function getTouches(evt) {
+            return evt.touches ||             // browser API
+                evt.originalEvent.touches; // jQuery
+        }
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];
+            xDown = firstTouch.clientX;
+            yDown = firstTouch.clientY;
+        };
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            let xUp = evt.touches[0].clientX;
+            let yUp = evt.touches[0].clientY;
+
+            let xDiff = xDown - xUp;
+            let yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* right swipe */
+                    ws.send('a')
+                } else {
+                    /* left swipe */
+                    ws.send('d')
+                }
+            } else {
+                if ( yDiff > 0 ) {
+                    /* down swipe */
+                    ws.send('s')
+                } else {
+                    /* up swipe */
+                    ws.send('w')
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        };
     }
 
     render() {
@@ -56,4 +105,8 @@ function sendInput(event){
             ws.send('d')
             break
     }
+}
+
+function mobileInputs(event){
+
 }
