f4a2ad9080e92dc87895151db015634a76e16917
Eberl Timo <te038@hdm-stuttgart.de>
2022-02-04 22:35:49 +0100

    changed package name to lowercase

--- server/src/main/java/networking/requests/GetHighscores.java
+++ server/src/main/java/networking/requests/GetHighscores.java
@@ -1,6 +1,6 @@
 package networking.requests;
 
-import Database.SQLConnection;
+import database.SQLConnection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
--- server/src/test/java/database/DatabaseTest.java
+++ server/src/test/java/database/DatabaseTest.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
-import Database.SQLConnection;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
