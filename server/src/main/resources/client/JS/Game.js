d52bf0bb7d15de6f24b1d51cc77b71da5efded14
Bauer Tim <tb166@hdm-stuttgart.de>
2022-02-05 11:44:09 +0100

    1. removing the eventListeners of the game input 2. if the database is closed, the main menue renders "failed to load"

--- server/src/main/resources/client/JS/Game.js
+++ server/src/main/resources/client/JS/Game.js
@@ -1,3 +1,5 @@
+let keyState = {}
+
 //define the Game with eventListener, the gameBoard and a player List with the current score
 class Game extends React.Component {
     componentDidMount() {
@@ -10,99 +12,20 @@ class Game extends React.Component {
         clearInterval(checkCurrentPlayers)
 
         //----------PC-INPUTS------------------------------
-        let keyState = {}
-
-        function onKeyPress(event) {
-                // keycode = event.code
-                let change = keyState[event.key] !== true
-                keyState[event.key] = true;
-
-                if (change) {
-                    console.clear()
-                    console.log("Press", event.key)
-                    handleInput(event.key);
-                }
-        }
-
-            function onKeyRelease(event) {
-                console.clear()
-                keyState[event.key] = false;
-                console.log("Release", event.key)
-                handleInput(null);
-            }
-
-            function handleInput(code) {
-                for (let c in keyState) {
-                    console.log(c, keyState[c])
-                }
-
-                if (code != null) {
-                    sendInput(code)
-                } else {
-                    for (let c in keyState) {
-                        if (true === keyState[c]) {
-                            console.error(c)
-                            sendInput(c)
-                            return
-                        }
-                    }
-                }
-        }
-
         document.addEventListener('keydown', onKeyPress)
         document.addEventListener('keyup', onKeyRelease)
 
         //----------MOBILE-INPUTS------------------------------
         document.addEventListener('touchstart', handleTouchStart, false);
         document.addEventListener('touchmove', handleTouchMove, false);
+    }
 
-        //Mobile Inputs from https://stackoverflow.com/users/7852/givanse
-        let xDown = null;
-        let yDown = null;
-
-        function getTouches(evt) {
-            return evt.touches ||             // browser API
-                evt.originalEvent.touches; // jQuery
-        }
-
-        function handleTouchStart(evt) {
-            const firstTouch = getTouches(evt)[0];
-            xDown = firstTouch.clientX;
-            yDown = firstTouch.clientY;
-        }
-
-        function handleTouchMove(evt) {
-            if ( ! xDown || ! yDown ) {
-                return;
-            }
-
-            let xUp = evt.touches[0].clientX;
-            let yUp = evt.touches[0].clientY;
-
-            let xDiff = xDown - xUp;
-            let yDiff = yDown - yUp;
-
-            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-                if ( xDiff > 0 ) {
-                    /* right swipe */
-                    ws.send('a')
-                } else {
-                    /* left swipe */
-                    ws.send('d')
-                }
-            } else {
-                if ( yDiff > 0 ) {
-                    /* down swipe */
-                    ws.send('w')
-                } else {
-                    /* up swipe */
-                    ws.send('s')
-                }
-            }
-            /* reset values */
-            xDown = null;
-            yDown = null;
-        }
+    componentWillUnmount(){
+        document.removeEventListener("keydown", sendInput)
+        document.removeEventListener("keydown", onKeyPress)
+        document.removeEventListener("keyup", onKeyRelease)
+        document.removeEventListener("touchstart", handleTouchStart)
+        document.removeEventListener("touchmove", handleTouchMove)
     }
 
     render() {
@@ -156,4 +79,90 @@ function sendInput(key){
             ws.send('d')
             break
     }
+}
+
+//----------PC-INPUTS------------------------------
+function onKeyPress(event) {
+    // keycode = event.code
+    let change = keyState[event.key] !== true
+    keyState[event.key] = true;
+
+    if (change) {
+        console.clear()
+        console.log("Press", event.key)
+        handleInput(event.key);
+    }
+}
+
+function onKeyRelease(event) {
+    console.clear()
+    keyState[event.key] = false;
+    console.log("Release", event.key)
+    handleInput(null);
+}
+
+function handleInput(code) {
+    for (let c in keyState) {
+        console.log(c, keyState[c])
+    }
+
+    if (code != null) {
+        sendInput(code)
+    } else {
+        for (let c in keyState) {
+            if (true === keyState[c]) {
+                console.error(c)
+                sendInput(c)
+                return
+            }
+        }
+    }
+}
+
+//Mobile Inputs from https://stackoverflow.com/users/7852/givanse
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */
+            ws.send('a')
+        } else {
+            /* left swipe */
+            ws.send('d')
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */
+            ws.send('w')
+        } else {
+            /* up swipe */
+            ws.send('s')
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
 }
